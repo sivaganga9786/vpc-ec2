b792856bde08e9076959451bc1df695f6420fca3
siva <siva@example.com>
2025-08-27 21:13:36 +0530

    destory

--- build.sh
+++ build.sh
@@ -1,29 +1,41 @@
 #!/bin/bash
 
-
 exec > /var/log/docker.log 2>&1
 set -xe
 
+# Update package index
+sudo apt-get update -y
 
-# Install Java 8, Java 11  Java 17 & Docker
-sudo apt update
-sudo apt-get install ca-certificates curl
+# Install prerequisites
+sudo apt-get install -y ca-certificates curl
+
+# Setup Docker repo
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
+
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
   $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
 
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+# Update apt repo with Docker packages
+sudo apt-get update -y
 
+# Install Docker
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
-## Install Kubectl
+# Enable and start Docker
+sudo systemctl enable docker
+sudo systemctl start docker
+
+# Install Kubectl
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+rm -f kubectl
 
+# Verify installs
+docker --version
 kubectl version --client
+
 
