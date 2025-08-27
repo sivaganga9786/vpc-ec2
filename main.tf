ad532d744848b81fb1a5f499a97db81f53912491
siva <siva@example.com>
2025-08-27 20:35:54 +0530

    create ec2 instance

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ module "vpc" {
 module "ec2" {
   source        = "git::https://github.com/sivaganga9786/Terraform-foundation.git//terraform-modules/ec2"
   instances      = var.instances
-  subnet_id      = module.vpc.public_subnets[0]
+  subnet_ids      = module.vpc.private_subnet_ids
   key_name       = var.key_name
   ec2_sg_name    = var.ec2_sg_name
   volume_size   = var.volume_size
@@ -23,3 +23,4 @@ module "ec2" {
     #file("${path.module}/ansible.sh")
   ]
 }
+
