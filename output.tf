b0afd3705de0c4e62f5fdc013b428aca81faf555
siva <siva@example.com>
2025-08-27 20:56:32 +0530

    errors resolved

--- output.tf
+++ output.tf
@@ -5,15 +5,3 @@ output "vpc_id" {
 output "ec2_instance_ids" {
   value = module.ec2.instance_ids
 }
-
-output "public_subnets" {
-  description = "IDs of the public subnets"
-  value       = aws_subnet.public[*].id
-}
-
-output "private_subnets" {
-  description = "IDs of the private subnets"
-  value       = aws_subnet.private[*].id
-}
-
-
